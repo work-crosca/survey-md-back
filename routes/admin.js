68980a021632a117c89a0dc3aaf4319ce78e65e2
FESK0 <work.crosca@gmail.com>
2025-06-02 14:29:43 +0300

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -19,7 +19,7 @@ router.post("/login", async (req, res) => {
     .cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     })
     .json({ success: true });
