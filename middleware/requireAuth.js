7bf16efd6e0d1af79474331849aa9332c311747a
FESK0 <work.crosca@gmail.com>
2025-06-02 14:23:37 +0300

    Update requireAuth.js

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -2,18 +2,20 @@ import jwt from "jsonwebtoken";
 import AdminUser from "../models/AdminUser.js";
 
 export const requireAuth = async (req, res, next) => {
-  const token = req.cookies.token;
-  if (!token) return res.status(401).json({ message: "Neautentificat" });
-
-  try {
-    const payload = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await AdminUser.findById(payload.id).select("-passwordHash");
-    if (!user) return res.status(401).json({ message: "Utilizator inexistent" });
-
-    req.user = user;
-    next();
-  } catch (err) {
-    console.error("Auth error:", err);
-    res.status(403).json({ message: "Token invalid sau expirat" });
-  }
-};
+   console.log("Cookies:", req.cookies);
+   const token = req.cookies.token;
+   if (!token) return res.status(401).json({ message: "Neautentificat" });
+ 
+   try {
+     const payload = jwt.verify(token, process.env.JWT_SECRET);
+     console.log("JWT payload:", payload);
+     const user = await AdminUser.findById(payload.id).select("-passwordHash");
+     if (!user) return res.status(401).json({ message: "Utilizator inexistent" });
+ 
+     req.user = user;
+     next();
+   } catch (err) {
+     console.error("Auth error:", err);
+     res.status(403).json({ message: "Token invalid sau expirat" });
+   }
+ };
